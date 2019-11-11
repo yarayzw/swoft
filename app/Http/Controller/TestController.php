a0fc762f9e8bb3d45f0269d719a964b4e29bef92
yarayzw <314850412@qq.com>
2019-11-11 12:31:26 +0800

    debug

--- app/Http/Controller/TestController.php
+++ app/Http/Controller/TestController.php
@@ -1,10 +1,18 @@
 <?php declare(strict_types=1);
+/**
+ * This file is part of Swoft.
+ *
+ * @link     https://swoft.org
+ * @document https://swoft.org/docs
+ * @contact  group@swoft.org
+ * @license  https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
 
 namespace App\Http\Controller;
 
-
 use Swoft\Http\Server\Annotation\Mapping\Controller;
 use Swoft\Http\Server\Annotation\Mapping\RequestMapping;
+use Throwable;
 
 /**
  * Class HomeController
@@ -12,14 +20,14 @@ use Swoft\Http\Server\Annotation\Mapping\RequestMapping;
  */
 class TestController
 {
-
-    /**a
+    /**
      * @RequestMapping("/test/api")
-     * @return string
+     * @throws Throwable
      */
-    public function testApi(): string
+    public function index()
     {
         return 'emmm';
     }
+
 
 }
